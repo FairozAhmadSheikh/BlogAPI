1ee7bf6bc746ef55274a5b5fdf11f5a5be6be057
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-09-14 12:25:00 +0530

    database connection

---
+++ database.py
@@ -0,0 +1,14 @@
+from sqlalchemy  import create_engine
+from sqlalchemy.orm import sessionmaker,declarative_base
+from dotenv import find_dotenv
+import os 
+
+find_dotenv()
+
+db_url=os.getenv("DATBASE_URL")
+
+
+
+engine=create_engine(os.getenv("DATBASE_URL"))
+SessionLocal=sessionmaker(bind=engine)
+base=declarative_base()
